f283f7026942aeb85eae0efbd6228311815107b3
Varshith Pabbisetty <krishnavarshith.pabbisetty@sjsu.edu>
2025-08-04 17:39:25 -0400

    REST API's and Integration Tests

---
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/controller/MoviesInfoController.java
@@ -0,0 +1,47 @@
+package com.reactivespring.moviesinfoservice.controller;
+
+import com.reactivespring.moviesinfoservice.domain.MovieInfo;
+import com.reactivespring.moviesinfoservice.service.MoviesInfoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/v1")
+public class MoviesInfoController {
+
+    private MoviesInfoService moviesInfoService;
+
+    public MoviesInfoController(MoviesInfoService moviesInfoService) {
+        this.moviesInfoService = moviesInfoService;
+    }
+
+    @GetMapping("/movieInfos")
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<MovieInfo> getAllMovieInfos() {
+        return moviesInfoService.getAllMovieInfos().log();
+    }
+
+    @GetMapping("/movieInfos/{movieInfoId}")
+    public Mono<MovieInfo> getMovieInfoById(@PathVariable String movieInfoId) {
+        return moviesInfoService.getMovieInfoById(movieInfoId).log();
+    }
+
+    @PostMapping("/movieInfos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo) {
+        return moviesInfoService.addMovieInfo(movieInfo).log();
+    }
+
+    @PutMapping("/movieInfos/{movieInfoId}")
+    public Mono<MovieInfo> updateMovieInfo(@PathVariable String movieInfoId, @RequestBody MovieInfo movieInfo) {
+        return moviesInfoService.updateMovieInfo(movieInfoId, movieInfo).log();
+    }
+
+    @DeleteMapping("movieInfos/{movieInfoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteMovieInfo(@PathVariable String movieInfoId) {
+        return moviesInfoService.deleteMovieInfo(movieInfoId);
+    }
+}
---
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/service/MoviesInfoService.java
@@ -0,0 +1,46 @@
+package com.reactivespring.moviesinfoservice.service;
+
+import com.reactivespring.moviesinfoservice.domain.MovieInfo;
+import com.reactivespring.moviesinfoservice.repository.MovieInfoRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
+@Service
+public class MoviesInfoService {
+
+    private MovieInfoRepository movieInfoRepository;
+
+    public MoviesInfoService(MovieInfoRepository movieInfoRepository) {
+        this.movieInfoRepository = movieInfoRepository;
+    }
+
+    public Mono<MovieInfo> addMovieInfo(MovieInfo movieInfo) {
+        return movieInfoRepository.save(movieInfo);
+    }
+
+    public Flux<MovieInfo> getAllMovieInfos() {
+        return movieInfoRepository.findAll();
+    }
+
+    public Mono<MovieInfo> getMovieInfoById(String movieInfoId) {
+        return movieInfoRepository.findById(movieInfoId);
+    }
+
+    public Mono<MovieInfo> updateMovieInfo(String movieInfoId, MovieInfo movieInfo) {
+        return movieInfoRepository.findById(movieInfoId)
+                .flatMap(movieInfo1 -> {
+                    movieInfo1.setCast(movieInfo.getCast());
+                    movieInfo1.setName(movieInfo.getName());
+                    movieInfo1.setReleaseDate(movieInfo.getReleaseDate());
+                    movieInfo1.setYear(movieInfo.getYear());
+                    return movieInfoRepository.save(movieInfo1);
+                });
+    }
+
+    public Mono<Void> deleteMovieInfo(String movieInfoId) {
+//        return movieInfoRepository.findById(movieInfoId)
+//                .flatMap(movieInfo1 -> movieInfoRepository.delete(movieInfo1));
+        return movieInfoRepository.deleteById(movieInfoId);
+    }
+}
