2d54380a1a508d9c6d454741b236a55166e06da4
Varshith Pabbisetty <p.krishnavarshith@temenos.com>
2025-08-07 10:38:26 -0400

    Functional/Reactive Programming REST APIs and Integration Tests

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -26,7 +26,45 @@ public class ReviewHandler {
     }
 
     public Mono<ServerResponse> getReviews(ServerRequest serverRequest) {
-        var reviewsFlux = reviewReactiveRepository.findAll();
+        var movieInfoId = serverRequest.queryParam("movieInfoId");
+
+        if (movieInfoId.isPresent()) {
+            var reviewsFlux = reviewReactiveRepository.findReviewsByMovieInfoId(Long.valueOf(movieInfoId.get()));
+            return buildReviewsResponse(reviewsFlux);
+        } else {
+            var reviewsFlux = reviewReactiveRepository.findAll();
+            return buildReviewsResponse(reviewsFlux);
+        }
+    }
+
+    private static Mono<ServerResponse> buildReviewsResponse(Flux<Review> reviewsFlux) {
         return ServerResponse.ok().body(reviewsFlux, Review.class);
+    }
+
+    public Mono<ServerResponse> updateReview(ServerRequest serverRequest) {
+        var reviewId = serverRequest.pathVariable("reviewId");
+
+        var existingReview = reviewReactiveRepository.findById(reviewId);
+
+        return existingReview
+                .flatMap(review -> serverRequest.bodyToMono(Review.class)
+                        .map(reqReview -> {
+                            review.setComment(reqReview.getComment());
+                            review.setRating(reqReview.getRating());
+                            return review;
+                        })
+                        .flatMap(reviewReactiveRepository::save)
+                        .flatMap(savedReview -> ServerResponse.ok().bodyValue(savedReview))
+                );
+
+    }
+
+    public Mono<ServerResponse> deleteReview(ServerRequest serverRequest) {
+        var reviewId = serverRequest.pathVariable("reviewId");
+
+        var existingReview = reviewReactiveRepository.findById(reviewId);
+
+        return existingReview.flatMap(review -> reviewReactiveRepository.deleteById(reviewId)
+                .then(ServerResponse.noContent().build()));
     }
 }
--- movies-review-service/src/test/java/intg/com/reactivespring/routes/ReviewsIntgTest.java
+++ movies-review-service/src/test/java/intg/com/reactivespring/routes/ReviewsIntgTest.java
@@ -10,8 +10,11 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -29,7 +32,7 @@ public class ReviewsIntgTest {
     @BeforeEach
     void setUp() {
         var reviewsList = List.of(
-                new Review(null, 1L, "Awesome Movie", 9.0),
+                new Review("1", 1L, "Awesome Movie", 9.0),
                 new Review(null, 1L, "Awesome Movie1", 9.0),
                 new Review(null, 2L, "Excellent Movie", 8.0));
         reviewReactiveRepository.saveAll(reviewsList)
@@ -44,7 +47,7 @@ public class ReviewsIntgTest {
     @Test
     void addReview() {
         //given
-        var review = new Review(null, 1L, "Awesome Movie", 9.0);
+        var review = new Review("1", 1L, "Awesome Movie", 9.0);
 
         //when
         webTestClient
@@ -79,5 +82,62 @@ public class ReviewsIntgTest {
                 .hasSize(3);
 
         //then
+    }
+
+    @Test
+    void updateReview() {
+        //given
+        var reviewId = "1";
+
+        var movieReview = new Review("1", 1L, "Awesome Movie Update", 9.0);
+
+        //when
+        webTestClient
+                .put()
+                .uri(MOVIES_REVIEW_URL + "/{reviewId}", reviewId)
+                .bodyValue(movieReview)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(Review.class)
+                .consumeWith(movieReviewEntityExchangeResult -> {
+                    var updatedMovieReview = movieReviewEntityExchangeResult.getResponseBody();
+                    assert updatedMovieReview != null;
+                    assert updatedMovieReview.getComment() != null;
+                    assertEquals("Awesome Movie Update", updatedMovieReview.getComment());
+                });
+        //then
+    }
+
+    @Test
+    void deleteReview() {
+        //given
+        var reviewId = "1";
+
+        //when
+        webTestClient
+                .delete()
+                .uri(MOVIES_REVIEW_URL + "/{reviewId}", reviewId)
+                .exchange()
+                .expectStatus()
+                .isNoContent();
+
+    }
+
+    @Test
+    void getReviewsByMovieInfoId() {
+        //given
+        var uri = UriComponentsBuilder.fromUriString(MOVIES_REVIEW_URL)
+                .queryParam("movieInfoId", 1L)
+                .buildAndExpand().toUri();
+
+        //when
+        webTestClient
+                .get()
+                .uri(uri)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Review.class);
     }
 }
